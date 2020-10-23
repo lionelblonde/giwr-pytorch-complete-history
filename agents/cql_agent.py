Correct uncertainty computation and use; add q and u logging

agents/cql_agent.py
@@ -298,10 +298,6 @@ class CQLAgent(object):
 
             action_from_actr = float(self.max_ac) * self.actr.sample(state, sg=False)
             log_prob = self.actr.logp(state, action_from_actr)
-            q_from_actr = self.crit.QZ(state, action_from_actr)
-            if self.hps.clipped_double:
-                twin_q_from_actr = self.twin.QZ(state, action_from_actr)
-                q_from_actr = torch.min(q_from_actr, twin_q_from_actr)
 
             # Only update the policy after a certain number of iteration (CQL codebase: 20000)
             # Note, as opposed to BEAR and BRAC, after the warm start, the BC loss is not used anymore
@@ -310,9 +306,20 @@ class CQLAgent(object):
 
             # Actor loss
             if start_using_q:
+                # Use full-blown loss
+                q_from_actr = self.crit.QZ(state, action_from_actr)
+                if self.hps.clipped_double:
+                    twin_q_from_actr = self.twin.QZ(state, action_from_actr)
+                    q_from_actr = torch.min(q_from_actr, twin_q_from_actr)
+                metrics['q_mean'].append(q_from_actr.mean())
+                metrics['q_std'].append(q_from_actr.std())
+                metrics['q_min'].append(q_from_actr.min())
+                metrics['q_max'].append(q_from_actr.max())
                 actr_loss = ((self.alpha_ent * log_prob) - q_from_actr).mean()
             else:
-                actr_loss = F.mse_loss(log_prob, self.actr.logp(state, action))
+                # Use behavioral cloning losses
+                actr_loss = (F.mse_loss(self.actr.logp(state, action).detach(), log_prob) +
+                             F.mse_loss(action, action_from_actr))
             metrics['actr_loss'].append(actr_loss)
 
             self.actr_opt.zero_grad()

agents/nets.py
@@ -377,12 +377,12 @@ class TanhGaussActor(nn.Module):
             ('fc_block_1', nn.Sequential(OrderedDict([
                 ('fc', nn.Linear(ob_dim, hidden_dims[0])),
                 ('ln', (nn.LayerNorm if hps.layer_norm else nn.Identity)(hidden_dims[0])),
-                ('nl', nn.Tanh()),
+                ('nl', nn.ReLU()),
             ]))),
             ('fc_block_2', nn.Sequential(OrderedDict([
                 ('fc', nn.Linear(hidden_dims[0], hidden_dims[1])),
                 ('ln', (nn.LayerNorm if hps.layer_norm else nn.Identity)(hidden_dims[1])),
-                ('nl', nn.Tanh()),
+                ('nl', nn.ReLU()),
             ]))),
         ]))
         if self.hps.state_dependent_std:

agents/orchestrator.py
@@ -240,12 +240,12 @@ def learn(args,
     timesteps_so_far = 0
     tstart = time.time()
 
-    # Create collections
-    d = defaultdict(list)
-    main_eval_deque = deque(maxlen=MAXLEN)
-    maxq_eval_deque = deque(maxlen=MAXLEN)
-
     if rank == 0:
+        # Create collections
+        d = defaultdict(list)
+        main_eval_deque = deque(maxlen=MAXLEN)
+        maxq_eval_deque = deque(maxlen=MAXLEN)
+
         # Set up model save directory
         ckpt_dir = osp.join(args.checkpoint_dir, experiment_name)
         os.makedirs(ckpt_dir, exist_ok=True)
@@ -347,11 +347,11 @@ def learn(args,
                     if training_step % args.pn_adapt_frequency == 0:
                         # Adapt parameter noise
                         agent.adapt_param_noise()
-                        if iters_so_far % args.eval_frequency == 0:
-                            # Store the action-space dist between perturbed and non-perturbed
-                            d['pn_dist'].append(agent.pn_dist)
-                            # Store the new std resulting from the adaption
-                            d['pn_cur_std'].append(agent.param_noise.cur_std)
+                    if rank == 0 and iters_so_far % args.eval_frequency == 0:
+                        # Store the action-space dist between perturbed and non-perturbed
+                        d['pn_dist'].append(agent.pn_dist)
+                        # Store the new std resulting from the adaption
+                        d['pn_cur_std'].append(agent.param_noise.cur_std)
 
                 # Sample a batch of transitions from the replay buffer
                 batch = agent.sample_batch()
@@ -362,10 +362,20 @@ def learn(args,
                     update_actor=not bool(iters_so_far % args.actor_update_delay),  # from TD3
                     iters_so_far=iters_so_far,
                 )
-                if iters_so_far % args.eval_frequency == 0:
+                if rank == 0 and iters_so_far % args.eval_frequency == 0:
                     # Log training stats
                     d['actr_losses'].append(metrics['actr_loss'])
                     d['crit_losses'].append(metrics['crit_loss'])
+                    if iters_so_far >= agent.hps.warm_start:
+                        d['q_stats_mean'].append(metrics['q_mean'])
+                        d['q_stats_std'].append(metrics['q_std'])
+                        d['q_stats_min'].append(metrics['q_min'])
+                        d['q_stats_max'].append(metrics['q_max'])
+                        if args.algo.split('_')[0] == 'ptso':
+                            d['u_stats_mean'].append(metrics['u_mean'])
+                            d['u_stats_std'].append(metrics['u_std'])
+                            d['u_stats_min'].append(metrics['u_min'])
+                            d['u_stats_max'].append(metrics['u_max'])
                     if agent.hps.clipped_double:
                         d['twin_losses'].append(metrics['twin_loss'])
                     if agent.hps.prioritized_replay:
@@ -404,6 +414,17 @@ def learn(args,
 
             # Log stats in csv
             logger.record_tabular(_str, step)
+
+            if iters_so_far - 1 >= agent.hps.warm_start:
+                logger.record_tabular('q_stats_mean', np.mean(d['q_stats_mean']))
+                logger.record_tabular('q_stats_std', np.mean(d['q_stats_std']))
+                logger.record_tabular('q_stats_min', np.mean(d['q_stats_min']))
+                logger.record_tabular('q_stats_max', np.mean(d['q_stats_max']))
+                if args.algo.split('_')[0] == 'ptso':
+                    logger.record_tabular('u_stats_mean', np.mean(d['u_stats_mean']))
+                    logger.record_tabular('u_stats_std', np.mean(d['u_stats_std']))
+                    logger.record_tabular('u_stats_min', np.mean(d['u_stats_min']))
+                    logger.record_tabular('u_stats_max', np.mean(d['u_stats_max']))
             logger.record_tabular('main_eval_len', np.mean(d['main_eval_len']))
             logger.record_tabular('maxq_eval_len', np.mean(d['maxq_eval_len']))
             logger.record_tabular('main_eval_env_ret', np.mean(d['main_eval_env_ret']))
@@ -435,6 +456,18 @@ def learn(args,
             if agent.hps.clipped_double:
                 wandb.log({'twin_loss': np.mean(d['twin_losses'])},
                           step=step)
+            if iters_so_far - 1 >= agent.hps.warm_start:
+                wandb.log({'q_stats_mean': np.mean(d['q_stats_mean']),
+                           'q_stats_std': np.mean(d['q_stats_std']),
+                           'q_stats_min': np.mean(d['q_stats_min']),
+                           'q_stats_max': np.mean(d['q_stats_max'])},
+                          step=step)
+                if args.algo.split('_')[0] == 'ptso':
+                    wandb.log({'u_stats_mean': np.mean(d['u_stats_mean']),
+                               'u_stats_std': np.mean(d['u_stats_std']),
+                               'u_stats_min': np.mean(d['u_stats_min']),
+                               'u_stats_max': np.mean(d['u_stats_max'])},
+                              step=step)
             wandb.log({'main_eval_len': np.mean(d['main_eval_len']),
                        'maxq_eval_len': np.mean(d['maxq_eval_len']),
                        'main_eval_env_ret': np.mean(d['main_eval_env_ret']),
@@ -443,8 +476,8 @@ def learn(args,
                        'avg_maxq_eval_env_ret': np.mean(maxq_eval_deque)},
                       step=step)
 
-        # Clear the iteration's running stats
-        d.clear()
+            # Clear the iteration's running stats
+            d.clear()
 
     if not agent.hps.offline:
         # When running the algorithm online, save behavior policy model once we are done iterating

agents/ptso_agent.py
@@ -70,9 +70,9 @@ class PTSOAgent(object):
         if self.hps.clipped_double:
             # Create second ('twin') critic and target critic
             # TD3, https://arxiv.org/abs/1802.09477
-            self.twin = Critic(self.env, self.hps, self.rms_obs, hidden_dims[1], ube=True).to(self.device)
+            self.twin = Critic(self.env, self.hps, self.rms_obs, hidden_dims[1], ube=False).to(self.device)
             sync_with_root(self.twin)
-            self.targ_twin = Critic(self.env, self.hps, self.rms_obs, hidden_dims[1], ube=True).to(self.device)
+            self.targ_twin = Critic(self.env, self.hps, self.rms_obs, hidden_dims[1], ube=False).to(self.device)
             self.targ_twin.load_state_dict(self.twin.state_dict())
 
         self.hps.use_adaptive_alpha = None  # unused in this algorithm, make sure it can not interfere
@@ -94,7 +94,7 @@ class PTSOAgent(object):
         self.targ_ent = -self.ac_dim
 
         # Initialize the precision matrix
-        self.precision = torch.eye(hidden_dims[1][-1])
+        self.precision = 5. * torch.eye(hidden_dims[1][-1])
 
         # Set up replay buffer
         shapes = {
@@ -301,10 +301,6 @@ class PTSOAgent(object):
 
             action_from_actr = float(self.max_ac) * self.actr.sample(state, sg=False)
             log_prob = self.actr.logp(state, action_from_actr)
-            q_from_actr = self.crit.QZ(state, action_from_actr)
-            if self.hps.clipped_double:
-                twin_q_from_actr = self.twin.QZ(state, action_from_actr)
-                q_from_actr = torch.min(q_from_actr, twin_q_from_actr)
 
             # Only update the policy after a certain number of iteration (CQL codebase: 20000)
             # Note, as opposed to BEAR and BRAC, after the warm start, the BC loss is not used anymore
@@ -313,11 +309,33 @@ class PTSOAgent(object):
 
             # Actor loss
             if start_using_q:
-                actr_loss = ((self.alpha_ent * log_prob) - q_from_actr +
-                             (self.hps.ptso_u_scale *
-                              self.crit.wrap_with_u_head(self.crit.phi(state, action)).sqrt())).mean()
+                # Use full-blown loss
+                q_from_actr = self.crit.QZ(state, action_from_actr)
+                if self.hps.clipped_double:
+                    twin_q_from_actr = self.twin.QZ(state, action_from_actr)
+                    q_from_actr = torch.min(q_from_actr, twin_q_from_actr)
+                metrics['q_mean'].append(q_from_actr.mean())
+                metrics['q_std'].append(q_from_actr.std())
+                metrics['q_min'].append(q_from_actr.min())
+                metrics['q_max'].append(q_from_actr.max())
+                actr_loss = (self.alpha_ent * log_prob) - q_from_actr
+
+                u_from_actr = self.crit.wrap_with_u_head(self.crit.phi(state, action_from_actr))
+                if iters_so_far > 0:
+                    # Skip the square root wrapping initially, reasonable chance of being negative at iter 0
+                    u_from_actr = u_from_actr.sqrt()
+                metrics['u_mean'].append(u_from_actr.mean())
+                metrics['u_std'].append(u_from_actr.std())
+                metrics['u_min'].append(u_from_actr.min())
+                metrics['u_max'].append(u_from_actr.max())
+                any_nan = u_from_actr.isnan().any()
+                assert not any_nan
+                actr_loss -= self.hps.ptso_u_scale * u_from_actr
+                actr_loss = actr_loss.mean()
             else:
-                actr_loss = F.mse_loss(log_prob, self.actr.logp(state, action))
+                # Use behavioral cloning losses
+                actr_loss = (F.mse_loss(self.actr.logp(state, action).detach(), log_prob) +
+                             F.mse_loss(action, action_from_actr))
             metrics['actr_loss'].append(actr_loss)
 
             self.actr_opt.zero_grad()
@@ -493,25 +511,42 @@ class PTSOAgent(object):
         if self.hps.clipped_double:
             twin_loss += min_twin_loss
 
-        # >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-
+        # Piece #4 (PTSO): uncertainty via Uncertainty Bellman Equation
         # The target of the Uncertainty Bellman Equation is the variance estimate of the error epsilon (paper)
+
+        # We don't use the twin critic past this point, to same on evalutations and matrix multiplications
+
         assert not self.hps.n_step_returns
 
         # Depending on a hyper-parameter, use the target critics in `targ_u` calculation, or not at all here
-        if self.hps.ptso_use_targ_for_u:
-            _crit = self.targ_crit
-            if self.hps.clipped_double:
-                _twin = self.targ_twin
-        else:
-            _crit = self.crit
-            if self.hps.clipped_double:
-                _twin = self.twin
+        _crit = self.targ_crit if self.hps.ptso_use_targ_for_u else self.crit
+
+        # Create the UBE target
+        # Computations done with batch dimension
+        with torch.no_grad():
+            new_phi = _crit.phi(state, action)  # shape: batch size x last hidden dim
+            new_next_phi = _crit.phi(next_state, next_action)  # shape: batch size x last hidden dim
+
+            _new_phi = new_phi.unsqueeze(-1)
+            _new_phi_t = torch.transpose(_new_phi, -1, -2)
+            inflated_precision = self.precision.unsqueeze(0).repeat(self.hps.batch_size, 1, 1)
+            targ_u = torch.bmm(torch.bmm(_new_phi_t,
+                                         inflated_precision),
+                               _new_phi).squeeze(-1)
+            targ_u += ((self.hps.gamma ** 2) *
+                       (1. - done) *
+                       _crit.wrap_with_u_head(new_next_phi))
+
+        u_crit_loss = F.mse_loss(self.crit.wrap_with_u_head(self.crit.phi(state, action).detach()),
+                                 targ_u.detach())  # detach, just in case
+
+        crit_loss += u_crit_loss
 
         # Update the global uncertainty matrix (sigma in UBE, the covariance matrix) according to equation 14 in UBE
         # Computations done without the batch dimension (non-batchable iterative process)
-        new_phi = _crit.phi(next_state, next_action).detach()  # shape: batch size x last hidden dim
-        for i in range(self.hps.batch_size):
+        n = int(np.clip(self.hps.ptso_num_mat_updates_per_iter, 1, self.hps.batch_size))
+        # Note, pick an n << batch_size to save compute time
+        for i in range(n):
             _new_phi = new_phi[i, :].unsqueeze(-1)
             _new_phi_t = torch.transpose(_new_phi, -1, -2)
             denom = (torch.matmul(torch.matmul(_new_phi_t,
@@ -523,38 +558,6 @@ class PTSOAgent(object):
                                               _new_phi_t),
                                  self.precision)
             self.precision -= (numer / denom)
-
-        # Create the UBE target
-        # Computations done with batch dimension
-        _new_phi = new_phi.unsqueeze(-1)
-        _new_phi_t = torch.transpose(_new_phi, -1, -2)
-        inflated_precision = self.precision.unsqueeze(0).repeat(self.hps.batch_size, 1, 1)
-        targ_u = torch.bmm(torch.bmm(_new_phi_t,
-                                     inflated_precision),
-                           _new_phi).squeeze(-1)
-        targ_u += ((self.hps.gamma ** 2) *
-                   (1. - done) *
-                   _crit.wrap_with_u_head(new_phi))
-        if self.hps.clipped_double:
-            _new_phi = _twin.phi(next_state, next_action).detach().unsqueeze(-1)
-            _new_phi_t = torch.transpose(_new_phi, -1, -2)
-            targ_u_twin = torch.bmm(torch.bmm(_new_phi_t,
-                                              inflated_precision),
-                                    _new_phi).squeeze(-1)
-            targ_u_twin += ((self.hps.gamma ** 2) *
-                            (1. - done) *
-                            _twin.wrap_with_u_head(new_phi))
-            targ_u = torch.min(targ_u, targ_u_twin)
-
-        u_crit_loss = F.mse_loss(self.crit.wrap_with_u_head(self.crit.phi(state, action)), targ_u.detach())
-        if self.hps.clipped_double:
-            u_twin_loss = F.mse_loss(self.twin.wrap_with_u_head(self.twin.phi(state, action)), targ_u.detach())
-
-        crit_loss += u_crit_loss
-        if self.hps.clipped_double:
-            twin_loss += u_twin_loss
-
-        # <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
 
         self.crit_opt.zero_grad()
         crit_loss.backward(retain_graph=True)